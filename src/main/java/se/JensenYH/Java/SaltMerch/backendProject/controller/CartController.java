451cf46ba6f7fedee1aeaa19cb2ec6d2f7b68933
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-01-09 12:29:01 +0100

    changes

--- src/main/java/se/JensenYH/Java/SaltMerch/backendProject/controller/CartController.java
+++ src/main/java/se/JensenYH/Java/SaltMerch/backendProject/controller/CartController.java
@@ -1,8 +1,14 @@
 package se.JensenYH.Java.SaltMerch.backendProject.controller;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.relational.core.mapping.Embedded;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import se.JensenYH.Java.SaltMerch.backendProject.model.CartItem;
 import se.JensenYH.Java.SaltMerch.backendProject.service.CartService;
+
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -12,14 +18,21 @@ public class CartController {
     @Autowired
     CartService cartService;
     @GetMapping("/selectall")
-    public List<CartItem> selectAllIteams(){
-        return cartService.selectAllItems();
+    public ResponseEntity<List<CartItem>> selectAllIteams(){
+
+        List<CartItem> cartItemList = cartService.selectAllItems();
+
+        if(cartItemList.isEmpty()){
+            return ResponseEntity.ok().header("list is emty").body(cartItemList);
+        }
+        return new  ResponseEntity<>(cartItemList, HttpStatus.OK);
     }
 
 
     //Almost done
-    @PostMapping("/iteam/{add}")
-    public void  addIteamToTheCart(CartItem item) {
+    @PostMapping("/iteam/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void  addIteamToTheCart(@RequestBody CartItem item) {
 
         cartService.insertOrIncrementItem(item);
 
